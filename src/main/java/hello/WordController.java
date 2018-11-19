36ab903d5ac7bb682518700fab77b68da97621eb
sangram shende <sangram.shende@pfizer.com>
2018-11-19 23:35:30 +0530

    TechM Assignment
    Completed assignment for TechM Selecion

--- src/main/java/hello/WordController.java
+++ src/main/java/hello/WordController.java
@@ -1,8 +1,68 @@
 package hello;
 
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import hello.reponseBean.PalindromeBean;
 
 @RestController
 public class WordController {
     // TODO Implement the /words/{word} endpoint
+	
+	@RequestMapping(value = "/words/{word}", produces = "application/json")
+	private PalindromeBean checkWord(@PathVariable("word")  String word) {		
+		PalindromeBean bean =  new PalindromeBean();
+		bean.setPalindrome(checkPalindrome(word));
+		bean.setAnagramOfPalindrome(checkAnagram(word));
+		bean.setWord(word);
+		return bean;
+	}
+	
+	/**
+	 * 
+	 * Below method is to check whether given string is Anagram of palindrome 
+	 * or not .
+	 * @param word - input String
+	 * 
+	 * **/
+	public static boolean checkAnagram(String word) {
+		int oddCount = 0;
+		word = word.toLowerCase();
+		int[] count = new int[256];
+ 		for (int i = 0; i < word.length(); i++) {
+			char ch = word.charAt(i);
+			count[ch]++;
+		}
+ 		
+ 		for (int currentCnt : count) {
+			if (oddCount > 1) {
+				return false;
+			}
+			if (currentCnt % 2 == 1) {
+				oddCount++;
+			}
+		}
+		return true;
+	}
+ 	
+	/**
+	 * 
+	 * Below method is to check whether given string is palindrome 
+	 * or not .
+	 * @param word - input String
+	 * 
+	 * **/
+	public static boolean checkPalindrome(String word) {
+		String revWord = "";
+		for (int i = word.length() - 1; i >= 0; i--)
+			revWord = revWord + word.charAt(i);
+	
+		if (word.toLowerCase().equals(revWord.toLowerCase())) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 }
+
---
+++ src/main/java/hello/reponseBean/PalindromeBean.java
@@ -0,0 +1,27 @@
+package hello.reponseBean;
+
+public class PalindromeBean {
+	private String word;
+	private boolean palindrome;
+	private boolean anagramOfPalindrome;
+	public String getWord() {
+		return word;
+	}
+	public void setWord(String word) {
+		this.word = word;
+	}
+	public boolean isPalindrome() {
+		return palindrome;
+	}
+	public void setPalindrome(boolean palindrome) {
+		this.palindrome = palindrome;
+	}
+	public boolean isAnagramOfPalindrome() {
+		return anagramOfPalindrome;
+	}
+	public void setAnagramOfPalindrome(boolean anagramOfPalindrome) {
+		this.anagramOfPalindrome = anagramOfPalindrome;
+	}
+	
+	
+ }
